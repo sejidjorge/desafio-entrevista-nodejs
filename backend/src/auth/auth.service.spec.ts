dbb2ca9cb00279fd363b934b5e505271d01fb881
Jorge Luis <79931563+sejidjorge@users.noreply.github.com>
2024-02-07 20:52:27 -0300

    feat: finish car and auth tests, finish doc api, update models *Finish all unity tests cars and authService *Finish all api documentation in swagger *Update models prisma

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,6 +1,16 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { AuthService } from "./auth.service";
 import { JwtService } from "@nestjs/jwt";
+import { $Enums } from "@prisma/client";
+import { AuthService } from "./auth.service";
+import { NextFunction, Request, Response } from "express";
+
+export class User {
+  id: string;
+  email: string;
+  name: string;
+  role: $Enums.Role;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 describe("AuthService", () => {
   let service: AuthService;
@@ -11,7 +21,34 @@ describe("AuthService", () => {
     service = new AuthService(jwtService);
   });
 
-  it("should be defined", () => {
-    expect(service).toBeDefined();
+  it("should return a token", async () => {
+    const user: User = {
+      id: "123e4567-e89b-12d3-a456-426614174000",
+      name: "John Doe",
+      email: "johndoe@email.com",
+      role: "USER",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    const token = "token";
+
+    jest.spyOn(service, "generateToken").mockImplementation(async () => token);
+
+    const actualUser = await service.generateToken(user);
+
+    expect(actualUser).toEqual(token);
+  });
+
+  it("should allow valid token", async () => {
+    const token =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHQiOjE3MDczMzgzNjB9.f3vxA6yQjrThL_JxNp9HdMAZaVkqAvvUfvUEKqnj_1o";
+    const req = { headers: { authorization: token } } as unknown as Request;
+    const res = {} as unknown as Response;
+    const next = jest.fn();
+
+    await service.validateSession(req, res, next);
+
+    expect(jwtService.verify).toHaveBeenCalledWith(token);
+    expect(next).toHaveBeenCalled();
   });
 });
--- backend/src/cars/cars.controller.spec.ts
+++ backend/src/cars/cars.controller.spec.ts
@@ -1,18 +1,164 @@
-import { Test, TestingModule } from "@nestjs/testing";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
 import { CarsController } from "./cars.controller";
+import { CarsService } from "./cars.service";
+import { HttpException, HttpStatus } from "@nestjs/common";
+
+const requestCreate = {
+  brand: "VW",
+  model: "Fusca",
+  year: "1969",
+  description: "this is a Fusca",
+  auctionStart: new Date(),
+  auctionEnd: new Date(),
+  startBid: 5000,
+  image: "https://images.pexels.com/photos/1209774/pexels-photo-1209774.jpeg",
+};
+const requestUpdate = {
+  brand: "VW",
+  model: "Fusca",
+  year: "1970",
+  description: "this is a Fusca",
+  auctionStart: new Date(),
+  auctionEnd: new Date(),
+  startBid: 5000,
+  image: "https://images.pexels.com/photos/1209774/pexels-photo-1209774.jpeg",
+};
+
+const expectedSingleResponse = {
+  id: "1",
+  brand: "VW",
+  model: "Fusca",
+  year: "1969",
+  description: "this is a Fusca",
+  auctionStart: new Date(),
+  auctionEnd: new Date(),
+  startBid: 5000,
+  image: "https://images.pexels.com/photos/1209774/pexels-photo-1209774.jpeg",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+const expectedListResponse = [
+  {
+    id: "1",
+    brand: "VW",
+    model: "Fusca",
+    year: "1969",
+    description: "this is a Fusca",
+    auctionStart: new Date(),
+    auctionEnd: new Date(),
+    startBid: 5000,
+    image: "https://images.pexels.com/photos/1209774/pexels-photo-1209774.jpeg",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: "2",
+    brand: "VW",
+    model: "Gol",
+    year: "1969",
+    description: "this is a Golf",
+    auctionStart: new Date(),
+    auctionEnd: new Date(),
+    startBid: 1000,
+    image:
+      "https://images.pexels.com/photos/14577491/pexels-photo-14577491.jpeg?",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
 
 describe("CarsController", () => {
-	let controller: CarsController;
+  let carsController: CarsController;
+  let carsService: CarsService;
+  let jwtService: JwtService;
 
-	beforeEach(async () => {
-		const module: TestingModule = await Test.createTestingModule({
-			controllers: [CarsController],
-		}).compile();
+  beforeEach(async () => {
+    carsService = new CarsService(jwtService);
+    carsController = new CarsController(carsService);
+  });
 
-		controller = module.get<CarsController>(CarsController);
-	});
+  it("should return a created car", async () => {
+    jest
+      .spyOn(carsService, "createCar")
+      .mockImplementation(async () => expectedSingleResponse);
 
-	it("should be defined", () => {
-		expect(controller).toBeDefined();
-	});
+    const request = {
+      body: { ...requestCreate },
+    } as unknown as Request;
+
+    const newCar = await carsController.createCar(request);
+    expect(newCar).toEqual(expectedSingleResponse);
+  });
+
+  it("should return a list of cars", async () => {
+    jest
+      .spyOn(carsService, "getCars")
+      .mockImplementation(async () => expectedListResponse);
+
+    const listCars = carsController.getCars();
+
+    expect(listCars).toEqual(expectedListResponse);
+  });
+
+  it("should return a car by id", async () => {
+    const carId = "1";
+    const request = {
+      params: {
+        carId,
+      },
+    } as unknown as Request;
+
+    jest
+      .spyOn(carsService, "getCar")
+      .mockImplementation(async () => expectedSingleResponse);
+
+    const getCar = carsController.getCar(request);
+
+    expect(getCar).toEqual(expectedSingleResponse);
+  });
+
+  it("should return a updated car", async () => {
+    const carId = "1";
+    const request = {
+      params: {
+        carId,
+      },
+      body: { ...requestUpdate },
+    } as unknown as Request;
+
+    jest
+      .spyOn(carsService, "updateCar")
+      .mockImplementation(async () => expectedSingleResponse);
+
+    const updateCar = carsController.updateCar(request);
+
+    expect(updateCar).toEqual(expectedSingleResponse);
+  });
+
+  it("should return a deleted car", async () => {
+    const carId = "1";
+    const request = {
+      params: {
+        carId,
+      },
+    } as unknown as Request;
+
+    const expectResponse: HttpException = new HttpException(
+      {
+        message: "Car deleted",
+        statusCode: HttpStatus.OK,
+      },
+      HttpStatus.OK
+    );
+
+    jest
+      .spyOn(carsService, "deleteCar")
+      .mockImplementation(async () => expectResponse);
+
+    const deleteCar = carsController.deleteCar(request);
+
+    expect(deleteCar).toEqual(expectedSingleResponse);
+  });
 });
